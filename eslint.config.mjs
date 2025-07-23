41a0aa1cec563e13854cc2404c79817a7bdd58af
Khokhych <bohdan.khokhych@gmail.com>
2025-07-23 18:52:30 +0300

    FIX: linter fix
    Signed-off-by: Khokhych <bohdan.khokhych@gmail.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,21 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: [
+      "**/lib/generated/**/*",
+      "**/prisma/migrations/**/*",
+      "**/.next/**/*",
+      "**/node_modules/**/*",
+    ],
+  },
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-require-imports": "off",
+    },
+  },
 ];
 
 export default eslintConfig;

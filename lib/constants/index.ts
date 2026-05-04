677b70d5507c3b70696b654e2ebb844f159e8066
Khokhych <bohdan.khokhych@gmail.com>
2026-05-04 14:11:15 +0300

    FIX: delete credit card
    Signed-off-by: Khokhych <bohdan.khokhych@gmail.com>

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -18,9 +18,9 @@ export const signUpDefaultValues = {
 
 export const PAYMENT_METHODS = process.env.PAYMENT_METHODS
   ? process.env.PAYMENT_METHODS.split(', ')
-  : ['PayPal', 'CreditCard', 'CashOnDelivery'];
+  : ['PayPal', 'CashOnDelivery'];
 export const DEFAULT_PAYMENT_METHOD =
-  process.env.DEFAULT_PAYMENT_METHOD || 'CreditCard';
+  process.env.DEFAULT_PAYMENT_METHOD || 'CashOnDelivery';
 
 export const PAGE_SIZE = Number(process.env.PAGE_SIZE) || 5;
 
